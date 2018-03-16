d9acde3131e981faee9be1d2e12639e319731e75
zhwls <zhwls@BeccasComputer>
2018-03-16 10:01:50 +0900

    requestmapping 최종수정 여기서시작

--- src/controllers/SampleContorller.java
+++ src/controllers/SampleContorller.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/foodie")
 public class SampleContorller {
 	
 	
@@ -13,16 +13,12 @@ public class SampleContorller {
 	public String sample() {
 		return "index";
 	}
-	@RequestMapping("/index2.do")
-	public String sample2() {
-		return "sample_index";
-	}
 	@RequestMapping("/main.do")
-	public String sample3() {
-		return "main";
+	public String sample2() {
+		return "mainpage";
 	}
 	@RequestMapping("/profile.do")
 	public String sample4() {
-		return "profile";
+		return "profilepage";
 	}
 }
