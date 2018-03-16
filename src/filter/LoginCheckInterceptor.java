df9b36cc80a4968f86a0baff40458967b5ce95eb
bluecrayonn <lucisama87@gmail.com>
2018-03-16 15:10:11 +0900

    Sangwon Park Branch  = 3/16
    1. login/logout controller/model 구현
    2. 해당하는 filter (구현 : path설정 남음)
    3. filter 랑 servlet 연결(구현 : path설정 남음)

--- src/filter/LoginCheckInterceptor.java
+++ src/filter/LoginCheckInterceptor.java
@@ -1,5 +1,33 @@
 package filter;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 public class LoginCheckInterceptor {
+	/*
+	 * redirect 경로는 login page로 
+	 * 
+	 */
+	
+	public boolean preHandler(HttpServletRequest req, HttpServletResponse resp) throws IOException	{
+		
+		HttpSession session = req.getSession(false);
+		
+		if(session == null) {
+			resp.sendRedirect("");
+			return false;
+		}
+		
+		String email = (String)session.getAttribute("auth");
+		if(email==null) {
+			resp.sendRedirect("");
+			return false;
+		}
+		
+		return true;
+	}
 
 }
